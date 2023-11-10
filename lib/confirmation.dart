d0e0e0ae9b8bfc696655ce998480808a201d5190
jif <jif@pop-os.localdomain>
2023-11-09 21:39:33 -0500

    WIP - working confirmation screen

--- lib/confirmation.dart
+++ lib/confirmation.dart
@@ -15,11 +15,55 @@ class Confirmation extends StatelessWidget {
   Widget build(BuildContext context) {
     debugPrint("In confirmation");
 
-    FinalPoll finalPoll = FinalPoll(polls: polls, contacts: selectedContacts);
+    //FinalPoll finalPoll = FinalPoll(polls: polls, contacts: selectedContacts);
     
-    sendPostRequest(finalPoll.toJson(), "submit/poll");
+    //sendPostRequest(finalPoll.toJson(), "submit/poll");
 
-    return const Text("In confirmation");
-    
+    return Scaffold(
+      body: ListView.builder( //TODO - too close to the top
+        padding: const EdgeInsets.all(8),
+        itemCount: polls.length,
+        itemBuilder: (context, index) =>
+          PollReview(poll: polls[index]),
+      )
+    );    
+  }
+}
+
+class PollReview extends StatelessWidget {
+  final Poll poll;
+
+  const PollReview({required this.poll});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: Column(
+          children: [ 
+            SizedBox(
+              width: 300,
+              height: 100,
+              child: Center(child: Text(poll.question!))
+            ),
+            ListView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: poll.answers!.length,
+              itemBuilder: (context, index) => 
+                SizedBox(
+                  width: 300,
+                  height: 100,
+                  child: Center(child: Text(poll.answers![index])),
+                  )
+                )
+          ]
+        )
+    );
   }
 }
