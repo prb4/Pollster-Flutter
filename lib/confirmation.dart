8148747645f772e75b04f13e1830ffb416c04c61
jif <jif@pop-os.localdomain>
2023-11-10 06:18:21 -0500

    Confirmation screen is functional

--- lib/confirmation.dart
+++ lib/confirmation.dart
@@ -20,11 +20,27 @@ class Confirmation extends StatelessWidget {
     //sendPostRequest(finalPoll.toJson(), "submit/poll");
 
     return Scaffold(
-      body: ListView.builder( //TODO - too close to the top
-        padding: const EdgeInsets.all(8),
-        itemCount: polls.length,
-        itemBuilder: (context, index) =>
-          PollReview(poll: polls[index]),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder( //TODO - too close to the top
+              padding: const EdgeInsets.all(8),
+              itemCount: polls.length,
+              itemBuilder: (context, index) =>
+                PollReview(poll: polls[index]),
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+          //                  Navigator.push(
+          //                    context,
+          //                    MaterialPageRoute(builder: (context) => const Creator()),
+          //                  ); 
+
+            },
+            child: const Text('Confirm'),
+          ),
+        ]
       )
     );    
   }
@@ -45,25 +61,27 @@ class PollReview extends StatelessWidget {
           ),
           borderRadius: const BorderRadius.all(Radius.circular(12)),
         ),
+        
         child: Column(
           children: [ 
-            SizedBox(
-              width: 300,
-              height: 100,
-              child: Center(child: Text(poll.question!))
-            ),
-            ListView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              itemCount: poll.answers!.length,
-              itemBuilder: (context, index) => 
-                SizedBox(
-                  width: 300,
-                  height: 100,
-                  child: Center(child: Text(poll.answers![index])),
-                  )
-                )
-          ]
+              Text(poll.question!),
+            //Expanded(
+              ListView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                shrinkWrap: true,
+                primary: false,
+                padding: const EdgeInsets.all(8),
+                itemCount: poll.answers!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return SizedBox(
+                    height: 50,
+                    child: Center(child: Text(poll.answers![index])),
+                  );
+                }
+              ),
+
+            ]
         )
-    );
-  }
+      );
+    }
 }
